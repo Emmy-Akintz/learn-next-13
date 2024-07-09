0f9706eb47d906734eae84609e7d9e8ba57a8e84
Ayomide Akintan <emzyakints2005@gmail.com>
2024-07-09 18:44:17 +0100

    tested static and dynamic rendering on production mode

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,14 +6,8 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('http://jsonplaceholder.typicode.com/users')
+  const res = await fetch('http://jsonplaceholder.typicode.com/users', { cache: 'no-store' })
   const users: User[] = await res.json()
-  // run 'npm run build'
-  // run 'npm start' to run the version built for production
-  // then observe what happens to the time when a client refreshs...
-  //!
-  // add an object to disable caching and then build the app again
-  // run it and observe the behaviour at refresh
   return (
     <>
       <h1>Users</h1>
